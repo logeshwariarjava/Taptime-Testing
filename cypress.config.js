0cb23bb071f36af23de7bbbd3ed9265460040a2c
logeshwariarjava <logeshwari515logu@gmail.com>
2025-11-24 15:52:31 +0530

    Update cypress testing setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: 'taptime-testing',
+  projectId: "uyrqmq",
   e2e: {
     baseUrl: 'https://taptime-testing.vercel.app',
     setupNodeEvents(on, config) {
