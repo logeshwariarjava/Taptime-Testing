1987c395686259f37f9cfc7a7e920226763f6e74
logeshwariarjava <logeshwari515logu@gmail.com>
2025-11-25 10:52:52 +0530

    code depolyed

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   projectId: "c52jbq",
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    
     setupNodeEvents(on, config) {
      
     },
