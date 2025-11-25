831433f93aa2cd3466d6fc20237d38bbc9ea87ff
logeshwariarjava <logeshwari515logu@gmail.com>
2025-11-25 11:11:06 +0530

    Fix baseUrl for cloud testing

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,9 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   projectId: "c52jbq",
   e2e: {
-    
+    baseUrl: 'https://taptime-react-logeshwariarjavas-projects.vercel.app',
     setupNodeEvents(on, config) {
      
     },
-    
   },
 })
-
